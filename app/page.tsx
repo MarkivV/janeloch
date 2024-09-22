b254d9d7de44d7b2004b50a4dfa33cb317e1c9f9
Volodymyr Markiv <67228698+MarkivV@users.noreply.github.com>
2024-09-22 21:31:15 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,17 @@ import { TeamSection } from "@/components/layout/sections/team";
 
 export const metadata = {
   title: "Seventons Software",
-  description: "Seventons Software",
+  description:
+    "SevenTons Software offers affordable data management and automation for small and medium businesses. Unlock insights, automate processes, and grow with our cost-effective solutions",
   openGraph: {
     type: "website",
-    url: "https://github.com/nobruf/shadcn-landing-page.git",
+    url: "https://www.seventons.com/",
     title: "Seventons Software",
-    description: "Seventons Software",
+    description:
+      "SevenTons Software offers affordable data management and automation for small and medium businesses. Unlock insights, automate processes, and grow with our cost-effective solutions",
     images: [
       {
-        url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
+        url: "/seventons2.png",
         width: 1200,
         height: 630,
         alt: "Seventons Software",
@@ -26,11 +28,17 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    site: "https://github.com/nobruf/shadcn-landing-page.git",
+    site: "https://www.seventons.com/",
     title: "Seventons Software",
-    description: "Seventons Software",
+    description:
+      "SevenTons Software offers affordable data management and automation for small and medium businesses. Unlock insights, automate processes, and grow with our cost-effective solutions",
     images: [
-      "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
+      {
+        url: "/seventons2.png",
+        width: 1200,
+        height: 630,
+        alt: "Seventons Software",
+      },
     ],
   },
 };
